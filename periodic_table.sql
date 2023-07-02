b5c74d461be6af68e2babca8b121e7783d0d9c09
jakeinvader <jakeinvader2@hotmail.com>
2023-07-02 17:08:13 +0000

    feat: upload periodic table database finished

--- periodic_table.sql
+++ periodic_table.sql
@@ -63,7 +63,7 @@ ALTER TABLE public.elements OWNER TO freecodecamp;
 CREATE TABLE public.properties (
     atomic_number integer NOT NULL,
     type_id integer NOT NULL,
-    atomic_mass numeric(9,6) NOT NULL,
+    atomic_mass numeric NOT NULL,
     melting_point_celsius numeric NOT NULL,
     boiling_point_celsius numeric NOT NULL
 );
@@ -132,16 +132,16 @@ INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (1, 1, 1.008000, -259.1, -252.9);
-INSERT INTO public.properties VALUES (2, 1, 4.002600, -272.2, -269);
-INSERT INTO public.properties VALUES (3, 2, 6.940000, 180.54, 1342);
-INSERT INTO public.properties VALUES (4, 2, 9.012200, 1287, 2470);
-INSERT INTO public.properties VALUES (5, 3, 10.810000, 2075, 4000);
-INSERT INTO public.properties VALUES (6, 1, 12.011000, 3550, 4027);
-INSERT INTO public.properties VALUES (7, 1, 14.007000, -210.1, -195.8);
-INSERT INTO public.properties VALUES (8, 1, 15.999000, -218, -183);
-INSERT INTO public.properties VALUES (9, 1, 18.998000, -220, -188.1);
-INSERT INTO public.properties VALUES (10, 1, 20.180000, -248.6, -246.1);
+INSERT INTO public.properties VALUES (2, 1, 4.0026, -272.2, -269);
+INSERT INTO public.properties VALUES (4, 2, 9.0122, 1287, 2470);
+INSERT INTO public.properties VALUES (1, 1, 1.008, -259.1, -252.9);
+INSERT INTO public.properties VALUES (3, 2, 6.94, 180.54, 1342);
+INSERT INTO public.properties VALUES (5, 3, 10.81, 2075, 4000);
+INSERT INTO public.properties VALUES (6, 1, 12.011, 3550, 4027);
+INSERT INTO public.properties VALUES (7, 1, 14.007, -210.1, -195.8);
+INSERT INTO public.properties VALUES (8, 1, 15.999, -218, -183);
+INSERT INTO public.properties VALUES (9, 1, 18.998, -220, -188.1);
+INSERT INTO public.properties VALUES (10, 1, 20.18, -248.6, -246.1);
 
 
 --
